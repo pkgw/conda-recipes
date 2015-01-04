eb6bef050d5518e8c1ac8c46763540910eafd859
Peter Williams <peter@newton.cx>
2015-01-04 16:58:04 -0500

    dbus-client: systemd-related build fix

--- dbus-client/build.sh
+++ dbus-client/build.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
+#
+# We disable systemd since otherwise the installation tries to write to
+# /usr/lib/... even though we've specified --prefix.
 
 set -e
-./configure --prefix=$PREFIX --disable-tests || { cat config.log ; exit 1 ; }
+./configure --prefix=$PREFIX --disable-tests --disable-systemd || { cat config.log ; exit 1 ; }
 make
 make install
 
