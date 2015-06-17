57a9b023473adc8cdaf3638ad8f1aeb89e924089
Peter Williams <peter@newton.cx>
2015-06-17 09:51:23 -0400

    aoflagger: update for version 2.7.0

--- aoflagger/build.sh
+++ aoflagger/build.sh
@@ -5,6 +5,8 @@
 set -e
 
 cmake_args="
+-DBLAS_LIBRARIES=$PREFIX/lib/libatlas.a
+-DLAPACK_LIBRARIES=$PREFIX/lib/liblapack.a;$PREFIX/lib/libf77blas.a;$PREFIX/lib/libatlas.a
 -DCMAKE_BUILD_TYPE=Release
 -DCMAKE_COLOR_MAKEFILE=OFF
 -DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath-link,$PREFIX/lib
