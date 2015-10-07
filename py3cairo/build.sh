1de83843cac19976d6508c05876bcb84af910915
Peter Williams <peter@newton.cx>
2015-10-07 12:45:29 -0400

    py3cairo: import
    This hasn't been updated in forever and so needs a bunch of patches to build,
    which I've stolen from Debian. The patch that adds the Region object doesn't
    work, even though it looks like everything is being done OK. I can't figure
    out what's going on there and there's only so much time I want to spend on this.

---
+++ py3cairo/build.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# Copyright 2015 Peter Williams and collaborators.
+# This file is licensed under a 3-clause BSD license; see LICENSE.txt.
+
+set -e
+./waf --help >/dev/null # trigger unpacking of waflib directory
+rdir=$(cd $RECIPE_DIR && pwd)
+(cd .waf* && patch -p1 -i $rdir/60_python-config-without-interpreter.patch)
+(cd .waf* && patch -p1 -i $rdir/70_dont-link-libpython.patch)
+(cd .waf* && patch -p1 -i $rdir/80_fix-pickle.patch)
+(cd .waf* && patch -p1 -i $rdir/81_pickling-again.patch)
+./waf configure --prefix=$PREFIX
+./waf build
+./waf install
+
+cd $PREFIX
+# touchups?
