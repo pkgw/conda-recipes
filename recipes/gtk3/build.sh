72d51e6c3a9a6f697bcda1908ec489ea724e5f2e
Peter Williams <peter@newton.cx>
2015-12-10 09:24:27 -0500

    recipes/gtk3: update to 3.14.15
    We can't update to the newest version because we're held back by Conda's version
    of Cairo. Which I would like to keep using if possible.

--- recipes/gtk3/build.sh
+++ recipes/gtk3/build.sh
@@ -3,12 +3,21 @@
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
 set -e
+# conda provides libffi, but it has a busted .la file:
+rm -f $PREFIX/lib/libffi.la
+
+export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig"
+export LDFLAGS="-L$PREFIX/lib"
 ./configure --prefix=$PREFIX --disable-gtk-doc || { cat config.log ; exit 1 ; }
 make -j$(nproc --ignore=4)
 make install
 
 cd $PREFIX
-rm -rf share/applications share/gtk-doc bin/gtk3-demo* bin/gtk3-widget-factory
-
-# workaround for libffi .la file path issues
-sed -i -e 's|lib/\.\./lib64|lib|g' lib/*.la lib/gtk-3.0/*/*/*.la
+rm -rf $(echo "
+share/applications
+share/gtk-doc
+share/man
+bin/gtk3-demo*
+bin/gtk3-icon-browser
+bin/gtk3-widget-factory
+")
