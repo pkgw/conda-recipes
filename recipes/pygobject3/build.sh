8747967cf6713148c023324fc5421ccc6bd4ca7e
Peter Williams <peter@newton.cx>
2015-12-10 09:28:20 -0500

    recipes/pygobject3: update to 3.18.2

--- recipes/pygobject3/build.sh
+++ recipes/pygobject3/build.sh
@@ -3,17 +3,16 @@
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
 set -e
-# I want to avoid local-OS packages, but cairo requires things like "xrender"
-# and "x11" and I so do not want to go down that rathole.
-###export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig"
-# conda libffi doesn't come with pkgconfig info:
+# conda provides libffi, but it has a busted .la file:
+rm -f $PREFIX/lib/libffi.la
+# ... and no pkgconfig info:
 export FFI_CFLAGS="-I$PREFIX/include" FFI_LIBS="-L$PREFIX/lib -lffi"
-./configure --prefix=$PREFIX --disable-gtk-doc || { cat config.log ; exit 1 ; }
+# Make sure we pick up local cairo, etc:
+export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig"
+
+./configure --prefix=$PREFIX || { cat config.log ; exit 1 ; }
 make -j$(nproc --ignore=4)
 make install
 
 cd $PREFIX
 rm -rf share/gtk-doc
-
-# workaround for libffi .la file path issues
-sed -i -e 's|lib/\.\./lib64|lib|g' lib/python*/site-packages/gi/*.la
