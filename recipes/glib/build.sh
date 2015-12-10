dc5371ee1e23e5839dd1a2e76c3b465a2fc0cf4a
Peter Williams <peter@newton.cx>
2015-12-09 23:38:23 -0500

    recipes/glib: update to 2.46.2

--- recipes/glib/build.sh
+++ recipes/glib/build.sh
@@ -3,14 +3,13 @@
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
 set -e
-# conda libffi doesn't come with pkgconfig info:
+# conda libffi doesn't come with pkgconfig info, and has a busted .la file:
+rm -f $PREFIX/lib/libffi.la
 export LIBFFI_CFLAGS="-I$PREFIX/include" LIBFFI_LIBS="-L$PREFIX/lib -lffi"
+
 ./configure --prefix=$PREFIX || { cat config.log ; exit 1 ; }
 make -j$(nproc --ignore=4)
 make install
 
 cd $PREFIX
 rm -rf share/bash-completion share/gdb share/gtk-doc share/systemtap
-
-# workaround for libffi .la file path issues
-sed -i -e 's|lib/\.\./lib64|lib|g' lib/*.la
