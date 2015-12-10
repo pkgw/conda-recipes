1cbbb282273cbeaee9e41282814c15b6f5f17c23
Peter Williams <peter@newton.cx>
2015-12-10 00:46:57 -0500

    recipes/at-spi2-core: update to 2.18.3
    While the configure script says it requires intltool >= 0.40.0, we build
    just fine without it, so add a patch to eliminate that requirement.
    
    We do however, require X. libexec/at-spi2-registryd requires libXtst, which I
    wasn't packaging before. It may not really be necessary, but it's not a big
    deal to include it.

--- recipes/at-spi2-core/build.sh
+++ recipes/at-spi2-core/build.sh
@@ -3,6 +3,14 @@
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
 set -e
+# conda provides libffi, but it has a busted .la file:
+rm -f $PREFIX/lib/libffi.la
+# need this for dbus:
+export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig"
+# need this to detect X11:
+export CPPFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib -Wl,-rpath-link,$PREFIX/lib"
+
 ./configure --prefix=$PREFIX --disable-gtk-doc || { cat config.log ; exit 1 ; }
 make -j$(nproc --ignore=4)
 make install
@@ -10,5 +18,3 @@ make install
 cd $PREFIX
 rm -rf etc/xdg share/gtk-doc
 
-# workaround for libffi .la file path issues
-sed -i -e 's|lib/\.\./lib64|lib|g' lib/*.la
