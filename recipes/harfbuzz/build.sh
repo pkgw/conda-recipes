94255e5f5027bfbe8df3bec143008d81b6763b11
Peter Williams <peter@newton.cx>
2015-12-10 08:53:53 -0500

    recipes/harfbuzz: update to 1.0.6

--- recipes/harfbuzz/build.sh
+++ recipes/harfbuzz/build.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 set -e
+# conda provides libffi, but it has a busted .la file:
+rm -f $PREFIX/lib/libffi.la
+
 ./configure --prefix=$PREFIX --disable-gtk-doc || { cat config.log ; exit 1 ; }
 make -j$(nproc --ignore=4)
 make install
 
 cd $PREFIX
 rm -rf share/gtk-doc
-
-# workaround for libffi .la file path issues
-sed -i -e 's|lib/\.\./lib64|lib|g' lib/*.la
