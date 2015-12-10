e5ee6b091b8e890e22061a4d34b2dc8418993903
Peter Williams <peter@newton.cx>
2015-12-09 23:56:24 -0500

    recipes/gobject-introspection: update to 1.46.0

--- recipes/gobject-introspection/build.sh
+++ recipes/gobject-introspection/build.sh
@@ -3,6 +3,8 @@
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
 set -e
+# conda provides libffi, but it has a busted .la file:
+rm -f $PREFIX/lib/libffi.la
 # don't get locally installed pkg-config entries:
 export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig"
 # conda libffi doesn't come with pkgconfig info:
@@ -10,7 +12,3 @@ export FFI_CFLAGS="-I$PREFIX/include" FFI_LIBS="-L$PREFIX/lib"
 ./configure --prefix=$PREFIX || { cat config.log ; exit 1 ; }
 make -j$(nproc --ignore=4)
 make install
-
-cd $PREFIX
-# workaround for libffi .la file path issues
-sed -i -e 's|lib/\.\./lib64|lib|g' lib/*.la lib/gobject-introspection/giscanner/*.la
