805f16b6c1e9197bbe2d2b1237cbd239d3ee1c30
Peter Williams <peter@newton.cx>
2015-12-10 00:02:53 -0500

    recipes/atk: update to 2.18.0

--- recipes/atk/build.sh
+++ recipes/atk/build.sh
@@ -3,12 +3,12 @@
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
 set -e
+# conda provides libffi, but it has a busted .la file:
+rm -f $PREFIX/lib/libffi.la
+
 ./configure --prefix=$PREFIX --disable-gtk-doc || { cat config.log ; exit 1 ; }
 make -j$(nproc --ignore=4)
 make install
 
 cd $PREFIX
 rm -rf share/gtk-doc
-
-# workaround for libffi .la file path issues
-sed -i -e 's|lib/\.\./lib64|lib|g' lib/*.la
