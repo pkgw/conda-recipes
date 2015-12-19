641b0944fea06470a64f7d15e91bab20c89b6393
Peter Williams <peter@newton.cx>
2015-12-19 18:46:46 -0500

    recipes/expat: ditto

--- recipes/expat/build.sh
+++ recipes/expat/build.sh
@@ -2,7 +2,9 @@
 # Copyright 2014-2015 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
+[ "$NJOBS" = '<UNDEFINED>' ] && NJOBS=1
 set -e
+
 ./configure --prefix=$PREFIX || { cat config.log ; exit 1 ; }
-make -j$(nproc --ignore=4)
+make -j$NJOBS
 make install
