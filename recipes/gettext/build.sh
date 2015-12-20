768432986e629b5ebf1393404399d77c4eae9ea2
Peter Williams <peter@newton.cx>
2015-12-20 17:52:59 -0500

    Work on portability of glib
    First of all, we were embedding deps on the Brew /usr/local version of libintl. It
    looks like I've really got to add my own gettext package, so let's go and do that.
    
    Then stock glib recently added a requirement of OS X 10.9 or higher. That's pretty
    aggressive. It's all for one GIo notification subsystem, so I've just disabled it.
    We'll see if anything breaks.

---
+++ recipes/gettext/build.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+# Copyright 2015 Peter Williams and collaborators.
+# This file is licensed under a 3-clause BSD license; see LICENSE.txt.
+
+[ "$NJOBS" = '<UNDEFINED>' ] && NJOBS=1
+set -e
+
+if [ -n "$OSX_ARCH" ] ; then
+    export MACOSX_DEPLOYMENT_TARGET=10.6
+    sdk=/SDKs/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk
+    export CFLAGS="$CFLAGS -isysroot $sdk"
+    export LDFLAGS="$LDFLAGS -Wl,-syslibroot,$sdk"
+else
+    echo only build me on OS X
+    exit 1
+fi
+
+./configure --prefix=$PREFIX || { cat config.log ; exit 1 ; }
+make -j$NJOBS
+make install
+
+cd $PREFIX
+rm -rf share/doc share/emacs share/info share/man
+
+
+
--- recipes/glib/build.sh
+++ recipes/glib/build.sh
@@ -6,10 +6,22 @@
 set -e
 
 if [ -n "$OSX_ARCH" ] ; then
-    # Need pkg-config, gettext.
-    export CPPFLAGS="-I/usr/local/opt/gettext/include" LDFLAGS="-L/usr/local/opt/gettext/lib"
-    export PATH="/usr/local/opt/gettext/bin:$PATH"
-    # OS X: libffi is provided by system
+    # GLib has builtin Cocoa support that requires OS X version 10.9 or
+    # greater. I could patch it out, but I don't have a sense of how much of
+    # an issue this will be in practice. If there are complaints from users of
+    # <= 10.8 I'll revisit.
+    export MACOSX_DEPLOYMENT_TARGET=10.7
+    sdk=/SDKs/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk
+    export CFLAGS="$CFLAGS -isysroot $sdk"
+    export LDFLAGS="$LDFLAGS -Wl,-syslibroot,$sdk"
+
+    # Pick up the Conda version of gettext/libintl:
+    export PATH="$PREFIX/bin:$PATH"
+    export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
+    export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
+
+    # OS X: we assume there's a pkg-config from Brew; libffi is provided by
+    # the system.
     export LIBFFI_CFLAGS="-I/usr/include/ffi" LIBFFI_LIBS="-lffi"
 else
     # Linux: conda provides libffi with a busted .la file:
