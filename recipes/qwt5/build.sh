061c13a3d906d2b850c4d486be8eb711e167cdc8
Peter Williams <peter@newton.cx>
2015-12-20 00:40:40 -0500

    recipes/qwt5: build on OS X

--- recipes/qwt5/build.sh
+++ recipes/qwt5/build.sh
@@ -2,14 +2,22 @@
 # Copyright 2015 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
+[ "$NJOBS" = '<UNDEFINED>' ] && NJOBS=1
 set -e
+
+if [ -n "$OSX_ARCH" ] ; then
+    spec=macx-g++
+else
+    spec=linux-g++
+fi
+
 cat <<EOF >>qwtconfig.pri
 target.path = $PREFIX/lib
 headers.path = $PREFIX/include/qwt\$\$VER_MAJ
 doc.path = $PREFIX/doc
 EOF
-qmake -spec linux-g++ qwt.pro
-make -j$(nproc --ignore=4)
+qmake -spec $spec qwt.pro
+make -j$NJOBS
 make install
 
 cd $PREFIX
