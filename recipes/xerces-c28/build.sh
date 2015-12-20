cd310e5bbea590a1d8aa37750c47d316251543a0
Peter Williams <peter@newton.cx>
2015-12-19 19:10:29 -0500

    recipes/xerces-c28: patch up for OS X build
    Was scary but fortunately there's a patch out there that fixes the OS X stuff.

--- recipes/xerces-c28/build.sh
+++ recipes/xerces-c28/build.sh
@@ -2,9 +2,17 @@
 # Copyright 2014-2015 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
+[ "$NJOBS" = '<UNDEFINED>' ] && NJOBS=1
 set -e
+
+if [ -n "$OSX_ARCH" ] ; then
+    platform=macosx
+else
+    platform=linux
+fi
+
 export XERCESCROOT=$(pwd)
 cd src/xercesc
-bash ./runConfigure -plinux -cgcc -xg++ -minmem -nsocket -tnative -rpthread -b64 -P$PREFIX
+bash ./runConfigure -p$platform -cgcc -xg++ -minmem -nsocket -tnative -rpthread -b64 -P$PREFIX
 make # note: build is not parallel-compatible
 make install
