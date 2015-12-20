7b3f7cf8ac1d40b352f166ea8deab22304cd0524
Peter Williams <peter@newton.cx>
2015-12-20 00:04:49 -0500

    recipes/pgplot: build for OS X
    Here we venture into the scary land of compiling Fortran code on OS X. It
    looks like Homebrew has a package that provides GFortran and generally Does
    The Right Thing so hopefully this isn't so bad. But I worry about the binary
    compatibility situation.
    
    Also I converted this package to use modern-xorg-stack.

--- recipes/pgplot/build.sh
+++ recipes/pgplot/build.sh
@@ -2,9 +2,19 @@
 # Copyright 2015 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
+[ "$NJOBS" = '<UNDEFINED>' ] && NJOBS=1
 set -e
 
-./makemake . linux gfortran_gcc
+if [ -n "$OSX_ARCH" ] ; then
+    subtype=actually_osx
+    shlib=dylib
+    export MACOSX_DEPLOYMENT_TARGET=10.6
+else
+    subtype=gfortran_gcc
+    shlib=so
+fi
+
+./makemake . linux $subtype
 for f in png.h pngconf.h pnglibconf.h zlib.h zconf.h ; do
     ln -s $PREFIX/include/$f $f
 done
@@ -12,6 +22,6 @@ make
 
 mkdir -p $PREFIX/bin $PREFIX/lib $PREFIX/include/pgplot $PREFIX/share/pgplot
 cp -a pgxwin_server $PREFIX/bin/
-cp -a libcpgplot.a libpgplot.a libpgplot.so libtkpgplot.a $PREFIX/lib/
+cp -a libcpgplot.a libpgplot.a libpgplot.$shlib libtkpgplot.a $PREFIX/lib/
 cp -a grfont.dat rgb.txt $PREFIX/share/pgplot/
 cp -a cpgplot.h grpckg1.inc pgplot.inc tkpgplot.h $PREFIX/include/pgplot/
