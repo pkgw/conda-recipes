1614eed6c8187dc17d1c24d4953dd8464f7a0b2e
Peter Williams <peter@newton.cx>
2015-12-19 22:19:39 -0500

    recipes/eigen: build on OS X
    Not sure what the right response to CMake's complaint about setting an OS X
    deployment target is. I have a feeling this might be ABI-related stuff in
    which case I should probably not be so cavalier.

--- recipes/eigen/build.sh
+++ recipes/eigen/build.sh
@@ -2,11 +2,16 @@
 # Copyright 2015 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
+[ "$NJOBS" = '<UNDEFINED>' ] && NJOBS=1
 set -e
 
 cmake_args="
 -DCMAKE_INSTALL_PREFIX=$PREFIX
 "
+
+if [ -n "$OSX_ARCH" ] ; then
+    cmake_args="$cmake_args -DCMAKE_OSX_DEPLOYMENT_TARGET="
+fi
 
 mkdir build
 cd build
