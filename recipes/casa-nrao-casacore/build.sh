bce60bb0a817b9ac8ad96077d3dbfe8c54028924
Peter Williams <peter@newton.cx>
2015-12-20 03:15:42 -0500

    recipes/casa-nrao-casacore: build on OS X!
    I had to deploy some advanced bash tactics, but overall actually pretty
    straightforward to get working. Did need to figure out that I needed to
    manually supply the -stdlib=libc++ argument to the C++ compiler, though.

--- recipes/casa-nrao-casacore/build.sh
+++ recipes/casa-nrao-casacore/build.sh
@@ -2,38 +2,56 @@
 # Copyright 2015 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
+[ "$NJOBS" = '<UNDEFINED>' ] && NJOBS=1
 set -e
 
-# Downstream elements of the C++ code require C++11 to compile, and for binary
-# compatibility we need to compile casacore with C++11 as well. We can do this
-# even on a CentOS 5 machine thanks to the Red Hat "devtoolset" package.
-# However, we still need to compile with stock gfortran, to maintain binary
-# compatibility with the Conda FORTRAN stack. Fun times.
+# We do fancy things with Bash arrays because we need to pass command-line
+# options that contain internal spaces, and with pure string expansions
+# there's no way to avoid them getting split up.
 
-cmake_args="
--DBLAS_atlas_LIBRARY=$PREFIX/lib/libcblas.a;$PREFIX/lib/libatlas.a
--DBUILD_PYTHON=ON
--DBUILD_TESTING=OFF
--DCMAKE_BUILD_TYPE=Release
--DCMAKE_C_COMPILER=/usr/bin/gcc
--DCMAKE_COLOR_MAKEFILE=OFF
--DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/g++
--DCMAKE_Fortran_COMPILER=/usr/bin/gfortran
--DCMAKE_INSTALL_PREFIX=$PREFIX
--DDATA_DIR=$PREFIX/lib/casa/data
--DUSE_FFTW3=ON
--DUSE_HDF5=ON
--DUSE_THREADS=ON
--DCXX11=ON
--DCONDA_CASA_ROOT=$PREFIX/lib/casa
-"
-jflag=-j4
+cmake_args=(
+    -DBUILD_PYTHON=ON
+    -DBUILD_TESTING=OFF
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_COLOR_MAKEFILE=OFF
+    -DCMAKE_INSTALL_PREFIX=$PREFIX
+    -DDATA_DIR=$PREFIX/lib/casa/data
+    -DUSE_FFTW3=ON
+    -DUSE_HDF5=ON
+    -DUSE_THREADS=ON
+    -DCXX11=ON
+    -DCONDA_CASA_ROOT=$PREFIX/lib/casa
+)
+
+if [ -n "$OSX_ARCH" ] ; then
+    # Need to require 10.7 because of the C++11 features.
+    export MACOSX_DEPLOYMENT_TARGET=10.7
+    cmake_args+=(
+	-DCMAKE_CXX_FLAGS="-arch x86_64 -stdlib=libc++"
+	-DCMAKE_Fortran_COMPILER=/usr/local/bin/gfortran-4.2
+	-DCMAKE_OSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET
+	-DCMAKE_OSX_SYSROOT=/SDKs/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk
+    )
+else
+    # Downstream elements of the C++ code require C++11 to compile, and for
+    # binary compatibility we need to compile casacore with C++11 as well. We
+    # can do this even on a CentOS 5 machine thanks to the Red Hat
+    # "devtoolset" package. However, we still need to compile with stock
+    # gfortran, to maintain binary compatibility with the Conda FORTRAN stack.
+    # Fun times.
+
+    cmake_args+=(
+	-DCMAKE_C_COMPILER=/usr/bin/gcc
+	-DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/g++
+	-DCMAKE_Fortran_COMPILER=/usr/bin/gfortran
+    )
+fi
 
 cd casacore
 mkdir build
 cd build
-cmake $cmake_args ..
-make $jflag VERBOSE=1
+cmake "${cmake_args[@]}" ..
+make -j$NJOBS VERBOSE=1
 make install
 
 cd $PREFIX
