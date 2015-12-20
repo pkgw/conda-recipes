33424a1d85460c2f80f9ee7d5dc8decf1642e7c8
Peter Williams <peter@newton.cx>
2015-12-20 02:00:22 -0500

    recipes/libsakura: build for OS X
    OK, I think I figured out a bit about binary compatibility for OS X. It looks
    a lot like I need to rebuild all of my packages to target the older SDKs. It
    turns out that libsakura uses C++0x features that require a minimum OS version
    of 10.7, whereas the Conda packages seem to target a minimum of 10.6.
    Shouldn't be a huge deal either way.

--- recipes/libsakura/build.sh
+++ recipes/libsakura/build.sh
@@ -2,20 +2,31 @@
 # Copyright 2015 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
+[ "$NJOBS" = '<UNDEFINED>' ] && NJOBS=1
 set -e
-
-# Note that unlike some of the other packages, here we use the devtools gcc as
-# well as g++ since CentOS 5's gcc chokes on a "-march=native" switch used by
-# the build system.
 
 cmake_args="
 -DCMAKE_BUILD_TYPE=Release
 -DCMAKE_COLOR_MAKEFILE=OFF
--DCMAKE_C_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/gcc
--DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/g++
 -DCMAKE_INSTALL_PREFIX=$PREFIX
 -DCMAKE_MODULE_PATH=../cmake-modules
 "
+
+if [ -n "$OSX_ARCH" ] ; then
+    cmake_args="$cmake_args
+-DCMAKE_OSX_DEPLOYMENT_TARGET=10.7
+-DCMAKE_OSX_SYSROOT=/SDKs/MacOSX10.7.sdk
+"
+else
+    # Note that unlike some of the other packages, here we use the devtools
+    # gcc as well as g++ since CentOS 5's gcc chokes on a "-march=native"
+    # switch used by the build system.
+
+    cmake_args="$cmake_args
+-DCMAKE_C_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/gcc
+-DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/g++
+"
+fi
 
 mkdir condabuild
 cd condabuild
