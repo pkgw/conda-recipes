244940a92f6607227144ec5a69632b2da7aeb1eb
Peter Williams <peter@newton.cx>
2015-12-20 00:20:30 -0500

    recipes/rpfits: build for OS X

--- recipes/rpfits/build.sh
+++ recipes/rpfits/build.sh
@@ -2,6 +2,14 @@
 # Copyright 2014-2015 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
+[ "$NJOBS" = '<UNDEFINED>' ] && NJOBS=1
 set -e
+
+if [ -n "$OSX_ARCH" ] ; then
+    FC=gfortran-4.2
+else
+    FC=gfortran
+fi
+
 cd linux64
-make install PREFIX=$PREFIX FC=gfortran FFLAGS="-g -O -fno-automatic -Wall -fPIC" CFLAGS="-g -O -Wall -fPIC"
+make install PREFIX=$PREFIX FC=$FC FFLAGS="-g -O -fno-automatic -Wall -fPIC" CFLAGS="-g -O -Wall -fPIC"
