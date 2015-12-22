f742f687359e6b8c6a26d5bc64a5d829b0ce7ff0
Peter Williams <peter@newton.cx>
2015-12-22 15:58:19 -0500

    recipes/rpfits: make this a dylib to avoid libgfortran madness

--- recipes/rpfits/build.sh
+++ recipes/rpfits/build.sh
@@ -9,24 +9,38 @@ FFLAGS="-g -O -fno-automatic -Wall -fPIC"
 CFLAGS="-g -O -Wall -fPIC"
 
 if [ -n "$OSX_ARCH" ] ; then
+    # I want to create a library that doesn't drag around issues w.r.t. linking
+    # with libgfortran. AFAICT that means I need to make a dynamic library, which
+    # the Makefile totally doesn't support. Fortunately, this codebase is so simple
+    # that a manual build is easy!
+
     export MACOSX_DEPLOYMENT_TARGET=10.6
-    sdk=/SDKs/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk
-    CFLAGS="$CFLAGS -arch $OSX_ARCH -isysroot $sdk"
-    FFLAGS="$FFLAGS -arch $OSX_ARCH -isysroot $sdk"
-    LDFLAGS="$LDFLAGS -Wl,-syslibroot,$sdk"
-
+    sdk=/
     FC=gfortran-4.2
-    subdir=darwin
-else
-    FC=gfortran
-    subdir=linux64
-fi
 
-cd $subdir
-make install \
-     PREFIX=$PREFIX \
-     CC=gcc \
-     FC=$FC \
-     CFLAGS="$CFLAGS" \
-     FFLAGS="$FFLAGS" \
-     LDFLAGS="$LDFLAGS"
+    mkdir -p $PREFIX/bin $PREFIX/lib $PREFIX/include
+
+    $FC -static-libgfortran -dynamiclib -o $PREFIX/lib/librpfits.dylib \
+	-install_name '@rpath/librpfits.dylib' -arch $OSX_ARCH \
+	-compatibility_version 1.0.0 -current_version 1.0.0 \
+	-headerpad_max_install_names \
+	$FFLAGS -isysroot $sdk \
+	-mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET \
+	code/*.f code/utdate.c code/darwin/*.f
+
+    for bin in rpfex rpfhdr ; do
+	gcc -dynamic -o $PREFIX/bin/$bin \
+	    code/$bin.c $PREFIX/lib/librpfits.dylib
+    done
+
+    cp -a code/RPFITS.h code/rpfits.inc $PREFIX/include
+else
+    cd linux64
+    make install \
+	 PREFIX=$PREFIX \
+	 CC=gcc \
+	 FC=gfortran \
+	 CFLAGS="$CFLAGS" \
+	 FFLAGS="$FFLAGS" \
+	 LDFLAGS="$LDFLAGS"
+fi
