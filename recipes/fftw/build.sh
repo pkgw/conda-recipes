88fa10947158aa90826e23eccd4503b9fac3aa61
Peter Williams <peter@newton.cx>
2015-12-22 23:45:48 -0500

    recipes/fftw: fix up SDK situation, bump build number

--- recipes/fftw/build.sh
+++ recipes/fftw/build.sh
@@ -6,27 +6,27 @@
 [ "$NJOBS" = '<UNDEFINED>' ] && NJOBS=1
 set -e
 
-CONFIGURE="./configure --prefix=$PREFIX --enable-shared --enable-threads --disable-fortran"
+configure="./configure --prefix=$PREFIX --enable-shared --enable-threads --disable-fortran"
 
 if [ -n "$OSX_ARCH" ] ; then
     export MACOSX_DEPLOYMENT_TARGET=10.6
-    sdk=/SDKs/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk
+    sdk=/
     export CFLAGS="$CFLAGS -isysroot $sdk"
     export LDFLAGS="$LDFLAGS -Wl,-syslibroot,$sdk"
 fi
 
 # Single precision (fftw libraries have "f" suffix)
-$CONFIGURE --enable-float --enable-sse
+$configure --enable-float --enable-sse
 make -j$NJOBS
 make install
 
 # Long double precision (fftw libraries have "l" suffix)
-$CONFIGURE --enable-long-double
+$configure --enable-long-double
 make -j$NJOBS
 make install
 
 # Double precision (fftw libraries have no precision suffix)
-$CONFIGURE --enable-sse2
+$configure --enable-sse2
 make -j$NJOBS
 make install
 
