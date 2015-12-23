0d0737b28038681374990257a46154cb713bc881
Peter Williams <peter@newton.cx>
2015-12-23 18:11:45 -0500

    recipes/harfbuzz: build on OS X
    And also throw in some new dependencies and provided features, since when does
    that ever lead to problems down the line?

--- recipes/harfbuzz/build.sh
+++ recipes/harfbuzz/build.sh
@@ -1,11 +1,39 @@
 #! /bin/bash
+# Copyright 2014-2015 Peter Williams and collaborators.
+# This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
+[ "$NJOBS" = '<UNDEFINED>' ] && NJOBS=1
 set -e
-# conda provides libffi, but it has a busted .la file:
-rm -f $PREFIX/lib/libffi.la
 
-./configure --prefix=$PREFIX --disable-gtk-doc || { cat config.log ; exit 1 ; }
-make -j$(nproc --ignore=4)
+# don't get locally installed pkg-config entries:
+export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig"
+
+configure_args=(--prefix=$PREFIX --disable-gtk-doc --with-gobject)
+
+if [ -n "$OSX_ARCH" ] ; then
+    # rpath setting is often needed to run compiled autoconf test programs:
+    export MACOSX_DEPLOYMENT_TARGET=10.6
+    sdk=/
+    export CFLAGS="$CFLAGS -isysroot $sdk"
+    export LDFLAGS="$LDFLAGS -Wl,-syslibroot,$sdk -Wl,-rpath,$PREFIX/lib"
+
+    configure_args+=(--with-coretext)
+
+    # Ugh. install_name fixup currently needed; have to copy since
+    # install_name_tool patches in place and the files are hardlinked out of
+    # the pkgs tree!
+    for lib in freetype; do
+	lpath=$PREFIX/lib/lib${lib}.dylib
+	mv $lpath $lpath.tmp
+	cp $lpath.tmp $lpath
+	rm -f $lpath.tmp
+	iname=$(otool -D $lpath |sed -e '2!d')
+	install_name_tool -id @rpath/$iname $lpath
+    done
+fi
+
+./configure "${configure_args[@]}" || { cat config.log ; exit 1 ; }
+make -j$NJOBS
 make install
 
 cd $PREFIX
