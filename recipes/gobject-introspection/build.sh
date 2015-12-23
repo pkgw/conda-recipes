132d9d77038f50ce50a46d12c2df39c2dc2697da
Peter Williams <peter@newton.cx>
2015-12-23 17:17:53 -0500

    recipes/gobject-introspection: update to build on OS X

--- recipes/gobject-introspection/build.sh
+++ recipes/gobject-introspection/build.sh
@@ -2,13 +2,22 @@
 # Copyright 2014-2015 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
+[ "$NJOBS" = '<UNDEFINED>' ] && NJOBS=1
 set -e
-# conda provides libffi, but it has a busted .la file:
-rm -f $PREFIX/lib/libffi.la
+
 # don't get locally installed pkg-config entries:
-export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig"
-# conda libffi doesn't come with pkgconfig info:
-export FFI_CFLAGS="-I$PREFIX/include" FFI_LIBS="-L$PREFIX/lib"
+export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig"
+
+if [ -n "$OSX_ARCH" ] ; then
+    export MACOSX_DEPLOYMENT_TARGET=10.6
+    sdk=/
+    export CFLAGS="$CFLAGS -isysroot $sdk"
+    export LDFLAGS="$LDFLAGS -Wl,-syslibroot,$sdk -Wl,-rpath,$PREFIX/lib"
+fi
+
+# needed to pick up our libintl.h on OS X:
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+
 ./configure --prefix=$PREFIX || { cat config.log ; exit 1 ; }
-make -j$(nproc --ignore=4)
+make -j$NJOBS
 make install
