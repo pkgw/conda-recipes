0b90e7ed90b3b83ccb15798d038a6332e3906002
Peter Williams <peter@newton.cx>
2015-12-23 18:18:07 -0500

    recipes/pango: build on OS X

--- recipes/pango/build.sh
+++ recipes/pango/build.sh
@@ -2,12 +2,34 @@
 # Copyright 2014-2015 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
+[ "$NJOBS" = '<UNDEFINED>' ] && NJOBS=1
 set -e
-# conda provides libffi, but it has a busted .la file:
-rm -f $PREFIX/lib/libffi.la
+
+# don't get locally installed pkg-config entries:
+export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig"
+
+if [ -n "$OSX_ARCH" ] ; then
+    # rpath setting is often needed to run compiled autoconf test programs:
+    export MACOSX_DEPLOYMENT_TARGET=10.6
+    sdk=/
+    export CFLAGS="$CFLAGS -isysroot $sdk"
+    export LDFLAGS="$LDFLAGS -Wl,-syslibroot,$sdk -Wl,-rpath,$PREFIX/lib"
+
+    # Ugh. install_name fixup currently needed; have to copy since
+    # install_name_tool patches in place and the files are hardlinked out of
+    # the pkgs tree!
+    for lib in freetype png16; do
+	lpath=$PREFIX/lib/lib${lib}.dylib
+	mv $lpath $lpath.tmp
+	cp $lpath.tmp $lpath
+	rm -f $lpath.tmp
+	iname=$(otool -D $lpath |sed -e '2!d')
+	install_name_tool -id @rpath/$iname $lpath
+    done
+fi
 
 ./configure --prefix=$PREFIX --with-included-modules=yes || { cat config.log ; exit 1 ; }
-make -j$(nproc --ignore=4)
+make -j$NJOBS
 make install
 
 cd $PREFIX
