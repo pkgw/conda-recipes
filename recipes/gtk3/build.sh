2a27bfbbdc436e03f6a48df786c07919582cee00
Peter Williams <peter@newton.cx>
2015-12-23 18:45:10 -0500

    recipes/gtk3: build on OS X! Pretty straightforward all told
    Hopefully I won't be bitten by the lack of an X11 backend.

--- recipes/gtk3/build.sh
+++ recipes/gtk3/build.sh
@@ -2,14 +2,42 @@
 # Copyright 2014-2015 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
+[ "$NJOBS" = '<UNDEFINED>' ] && NJOBS=1
 set -e
-# conda provides libffi, but it has a busted .la file:
-rm -f $PREFIX/lib/libffi.la
 
-export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig"
+# don't get locally installed pkg-config entries:
+export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig"
+
+# To pick up X11 etc.:
 export LDFLAGS="-L$PREFIX/lib"
-./configure --prefix=$PREFIX --disable-gtk-doc || { cat config.log ; exit 1 ; }
-make -j$(nproc --ignore=4)
+
+configure_args=(--prefix=$PREFIX --disable-gtk-doc)
+
+if [ -n "$OSX_ARCH" ] ; then
+    # rpath setting is often needed to run compiled autoconf test programs:
+    export MACOSX_DEPLOYMENT_TARGET=10.6
+    sdk=/
+    export CFLAGS="$CFLAGS -isysroot $sdk"
+    export LDFLAGS="$LDFLAGS -Wl,-syslibroot,$sdk -Wl,-rpath,$PREFIX/lib"
+
+    # The X11 backend requires fontconfig, which we don't have on OS X.
+    configure_args+=(--disable-x11-backend --enable-quartz-backend)
+
+    # Ugh. install_name fixup currently needed; have to copy since
+    # install_name_tool patches in place and the files are hardlinked out of
+    # the pkgs tree!
+    for lib in freetype png16; do
+	lpath=$PREFIX/lib/lib${lib}.dylib
+	mv $lpath $lpath.tmp
+	cp $lpath.tmp $lpath
+	rm -f $lpath.tmp
+	iname=$(otool -D $lpath |sed -e '2!d')
+	install_name_tool -id @rpath/$iname $lpath
+    done
+fi
+
+./configure "${configure_args[@]}" || { cat config.log ; exit 1 ; }
+make -j$NJOBS
 make install
 
 cd $PREFIX
