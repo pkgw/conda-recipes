f66ab7450f1bc88e67286f35dcc39778dfd4194b
Peter Williams <peter@newton.cx>
2016-03-10 16:23:41 -0500

    recipes/miriad: add this
    Uses a tarball with a hacked-up build system that I should fix at some point.

---
+++ recipes/miriad/build.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+# Copyright 2015 Peter Williams and collaborators.
+# This file is licensed under a 3-clause BSD license; see LICENSE.txt.
+
+[ "$NJOBS" = '<UNDEFINED>' ] && NJOBS=1
+set -e
+
+if [ -n "$OSX_ARCH" ] ; then
+    export MACOSX_DEPLOYMENT_TARGET=10.6
+    sdk=/ # might be useful later?
+    export CFLAGS="$CFLAGS -isysroot $sdk"
+    export LDFLAGS="$LDFLAGS -Wl,-syslibroot,$sdk"
+else
+    # get newer gfortran that can do static libgfortran
+    export PATH="/opt/rh/devtoolset-2/root/usr/bin:/opt/rh/devtoolset-2/root/bin:$PATH"
+fi
+
+./configure --prefix=$PREFIX --with-deplibs=$PREFIX --with-telescope=big1 --bindir=$PREFIX/lib/miriad || { cat config.log ; exit 1 ; }
+make -j$NJOBS
+make install
