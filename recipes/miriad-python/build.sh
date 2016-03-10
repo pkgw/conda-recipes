2905350d82e0c056f7292f39ea5c131fcbf87e6e
Peter Williams <peter@newton.cx>
2016-03-10 17:09:45 -0500

    recipes/miriad-python: package this up

---
+++ recipes/miriad-python/build.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+# Copyright 2015 Peter Williams and collaborators.
+# This file is licensed under a 3-clause BSD license; see LICENSE.txt.
+
+[ "$NJOBS" = '<UNDEFINED>' ] && NJOBS=1
+set -e
+
+if [ -n "$OSX_ARCH" ] ; then
+    export MACOSX_DEPLOYMENT_TARGET=10.6
+    sdk=/ # might be useful later?
+    export CFLAGS="$CFLAGS -isysroot $sdk"
+    export LDFLAGS="$LDFLAGS -Wl,-syslibroot,$sdk"
+else
+    # get newer gfortran that can do static libgfortran
+    export PATH="/opt/rh/devtoolset-2/root/usr/bin:/opt/rh/devtoolset-2/root/bin:$PATH"
+fi
+
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$PREFIX/lib"
+
+./configure --prefix=$PREFIX --with-miriad=$PREFIX || { cat config.log ; exit 1 ; }
+make -j$NJOBS
+make install
