d61146b4956e21d62f8a8afe1d2ea4cc3b71a2d1
Peter Williams <peter@newton.cx>
2016-03-12 12:16:10 -0500

    recipes/aoflagger: rebuild against newer CASA

--- recipes/aoflagger/build.sh
+++ recipes/aoflagger/build.sh
@@ -2,24 +2,50 @@
 # Copyright 2015 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
+[ "$NJOBS" = '<UNDEFINED>' -o -z "$NJOBS" ] && NJOBS=1
 set -e
 
-cmake_args="
--DBLAS_LIBRARIES=$PREFIX/lib/libatlas.a
--DLAPACK_LIBRARIES=$PREFIX/lib/liblapack.a;$PREFIX/lib/libf77blas.a;$PREFIX/lib/libatlas.a
--DCMAKE_BUILD_TYPE=Release
--DCMAKE_COLOR_MAKEFILE=OFF
--DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath-link,$PREFIX/lib
--DCMAKE_INSTALL_PREFIX=$PREFIX
--DCMAKE_MODULE_LINKER_FLAGS=-Wl,-rpath-link,$PREFIX/lib
--DCMAKE_SHARED_LINKER_FLAGS=-Wl,-rpath-link,$PREFIX/lib
-"
-jflag=-j4
+cmake_args=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_COLOR_MAKEFILE=OFF
+    -DCMAKE_INSTALL_PREFIX=$PREFIX
+)
+
+#cmake_args+=(--debug-trycompile --debug-output)
+
+if [ -n "$OSX_ARCH" ] ; then
+    # Need to require 10.7 because of the C++11 features.
+    export MACOSX_DEPLOYMENT_TARGET=10.7
+
+    # TODO: any install_name fixes needed?
+
+    cmake_args+=(
+	-Darch=darwin64
+	-Darchflag=x86_64
+	-DCMAKE_CXX_FLAGS="-arch $OSX_ARCH -stdlib=libc++ -std=c++11"
+	-DCMAKE_Fortran_COMPILER=/usr/local/bin/gfortran-4.2
+	-DCMAKE_OSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET
+	-DCMAKE_OSX_SYSROOT=/
+    )
+else
+    toolroot=/opt/rh/devtoolset-2/root
+
+    cmake_args+=(
+	-DBLAS_LIBRARIES="$PREFIX/lib/libopenblas.so"
+	-DCMAKE_C_COMPILER=$toolroot/usr/bin/gcc
+	-DCMAKE_CXX_COMPILER=$toolroot/usr/bin/g++
+	-DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath-link,$PREFIX/lib
+	-DCMAKE_Fortran_COMPILER=$toolroot/usr/bin/gfortran
+	-DCMAKE_MODULE_LINKER_FLAGS=-Wl,-rpath-link,$PREFIX/lib
+	-DCMAKE_SHARED_LINKER_FLAGS=-Wl,-rpath-link,$PREFIX/lib
+	-DLAPACK_LIBRARIES="$PREFIX/lib/libopenblas.so"
+    )
+fi
 
 mkdir build
 cd build
-cmake $cmake_args ..
-make $jflag VERBOSE=1
+cmake "${cmake_args[@]}" ..
+make -j$NJOBS VERBOSE=1
 make install
 
 cd $PREFIX
