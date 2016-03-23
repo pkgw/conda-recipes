862895ec49cf19230d787edbe86b2aeaa1739dd1
Peter Williams <peter@newton.cx>
2016-03-23 18:35:51 -0400

    recipes/libffi32: update to build correctly on Linux
    Patching the autotools files to build a library with a different name turns out
    to actually be pretty easy.

--- recipes/libffi32/build.sh
+++ recipes/libffi32/build.sh
@@ -1,36 +1,39 @@
 #! /bin/bash
-# Copyright 2015 Peter Williams and collaborators.
+# Copyright 2016 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
 [ "$NJOBS" = '<UNDEFINED>' -o -z "$NJOBS" ] && NJOBS=1
 set -e
+
+configure_args=(
+    --prefix=$PREFIX
+)
 
 if [ -n "$OSX_ARCH" ] ; then
     export MACOSX_DEPLOYMENT_TARGET=10.6
     sdk=/
     export CFLAGS="$CFLAGS -isysroot $sdk"
     export LDFLAGS="$LDFLAGS -Wl,-syslibroot,$sdk"
+    sedinplace=(-i '')
+else
+    sedinplace=(--in-place=)
 fi
 
-./configure --prefix=$PREFIX || { cat config.log ; exit 1 ; }
+./configure "${configure_args[@]}" || { cat config.log ; exit 1 ; }
 make -j$NJOBS
 make install
-
-# Now we patch up the library name; whee
 
 cd $PREFIX
 rm -rf share
 rm -f lib/*.la lib/*.a
-sed -i '' -e 's|-lffi|-lffi32|' lib/pkgconfig/libffi.pc
+sed "${sedinplace[@]}" -e 's|-lffi|-lffi32|' lib/pkgconfig/libffi.pc
 
-if [ -n "$OSX_ARCH" ] ; then
-    vers=6
-    cd lib
-    mv libffi.$vers.dylib libffi32.$vers.dylib
-    rm -f libffi.dylib
-    ln -s libffi32.$vers.dylib libffi32.dylib
-    install_name_tool -id "$(pwd)/libffi32.$vers.dylib" libffi32.$vers.dylib
-else
-    echo need to implement SO renaming on non-OSX platform
-    exit 1
-fi
+# I think this should no longer be needed?
+###if [ -n "$OSX_ARCH" ] ; then
+###    vers=6
+###    cd lib
+###    mv libffi.$vers.dylib libffi32.$vers.dylib
+###    rm -f libffi.dylib
+###    ln -s libffi32.$vers.dylib libffi32.dylib
+###    install_name_tool -id "$(pwd)/libffi32.$vers.dylib" libffi32.$vers.dylib
+###fi
