a68dda6b5f10f152c3f19cd1c6b32e925804f838
Peter Williams <peter@newton.cx>
2016-03-23 19:55:10 -0400

    recipes/adwaita-icon-theme: add this
    We need to provide our own theme files for various Gtk+ things to actually
    work. I am worried because some styling information is propagated through
    X atoms and so people running on different distros may have issues if
    their outer sessions use a different theme, but I believe that everything
    will be OK if we provide that 'default' symlink. We'll see.

---
+++ recipes/adwaita-icon-theme/build.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# Copyright 2014-2016 Peter Williams and collaborators.
+# This file is licensed under a 3-clause BSD license; see LICENSE.txt.
+
+[ "$NJOBS" = '<UNDEFINED>' -o -z "$NJOBS" ] && NJOBS=1
+set -e
+
+# don't get locally installed pkg-config entries:
+export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig"
+
+configure_args=(--prefix=$PREFIX)
+
+./configure "${configure_args[@]}" || { cat config.log ; exit 1 ; }
+make -j$NJOBS
+make install
+
+cd $PREFIX
+cd share/icons
+ln -sf Adwaita default
