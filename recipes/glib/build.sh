081cada8df3910f9494c72e057ca60ff638b2c3f
Peter Williams <peter@newton.cx>
2016-03-23 20:27:24 -0400

    recipes/glib: fix up g_get_system_data_dirs() default response

--- recipes/glib/build.sh
+++ recipes/glib/build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# Copyright 2014-2015 Peter Williams and collaborators.
+# Copyright 2014-2016 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
 [ "$NJOBS" = '<UNDEFINED>' -o -z "$NJOBS" ] && NJOBS=1
@@ -7,6 +7,9 @@ set -e
 
 # don't get locally installed pkg-config entries:
 export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig"
+
+# Needed to get appropriate response to g_get_system_data_dirs():
+export CFLAGS="-DCONDA_SYSTEM_DATA_DIRS=\\\"$PREFIX/share\\\""
 
 if [ -n "$OSX_ARCH" ] ; then
     # Cf. the discussion in meta.yaml -- we require 10.7.
