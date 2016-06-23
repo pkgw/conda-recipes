ac42baf7234cb6134e173b38c5836ea2ce01f9b1
Peter Williams <peter@newton.cx>
2016-06-23 14:09:12 -0400

    dockerfiles/conda-py2-builder: add a simple build script

---
+++ dockerfiles/conda-py2-builder/build.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# Copyright 2015-2016 Peter Williams
+# Licensed under the MIT License.
+
+set -e
+
+base=conda-py2-builder
+tag=$(date +%Y%m%d)
+
+docker build -t $base:$tag .
+docker tag $base:$tag $base:latest
