6bf47de8cc4ea8ae13160d2f0e283d34db885486
Peter Williams <peter@newton.cx>
2016-10-06 13:58:32 -0400

    recipes/gsl: need to update GSL
    Conda now provides a version, and it's a higher version number than ours ... but it's
    32-bit, not 64-bit?? Sigh. Whatever. Keep this package around for now.

--- recipes/gsl/build.sh
+++ recipes/gsl/build.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
-# Copyright 2015 Peter Williams and collaborators.
+# Copyright 2015-2016 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
 [ "$NJOBS" = '<UNDEFINED>' -o -z "$NJOBS" ] && NJOBS=1
 set -e
+test $(echo "$PREFIX" |wc -c) -gt 200 # check that we're getting long paths
 
 if [ -n "$OSX_ARCH" ] ; then
     export MACOSX_DEPLOYMENT_TARGET=10.6
