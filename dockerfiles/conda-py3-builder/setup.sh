24139c5296457914f9791bead28c6eb71eb92f75
Peter Williams <peter@newton.cx>
2016-12-14 12:29:11 -0500

    dockerfiles/conda-py3-builder: add this
    Also includes some updates for disappearing Yum repos, etc.

---
+++ dockerfiles/conda-py3-builder/setup.sh
@@ -0,0 +1,88 @@
+#! /bin/bash
+# Copyright 2015-2016 Peter Williams <peter@newton.cx>
+# Licensed under the MIT License.
+#
+# Set up an image that's ready to build Conda packages repeatably.
+
+set -e -x
+
+# Centos 5 yum silliness
+echo 'exclude=*.i386 *.i586 *.i686' >>/etc/yum.conf
+yum update -y
+
+# Base packages (keep alphabetized!)
+yum install -y $(echo "
+bison
+bzip2
+curl
+emacs-nox
+epel-release
+file
+flex
+gcc
+gcc-c++
+gcc-gfortran
+gettext-devel
+java
+libstdc++-devel
+make
+man
+man-pages
+ncurses-devel
+patch
+perl-XML-Parser
+pkgconfig
+strace
+subversion
+tar
+tcsh
+wget
+which
+xz
+zip
+")
+
+# Can now install git because we got epel-release
+yum install -y $(echo "
+git
+")
+
+# Devtools
+wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo
+yum install -y $(echo "
+devtoolset-2-binutils
+devtoolset-2-elfutils
+devtoolset-2-gcc
+devtoolset-2-gcc-c++
+devtoolset-2-gcc-gfortran
+")
+
+# Python 3 Miniconda
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p /conda
+cat >~/.bashrc <<'EOF'
+# .bashrc
+alias l='ls -lart --color=auto'
+PS1='\h \A \W \$ '
+export PATH="/conda/bin:$PATH"
+EOF
+source ~/.bashrc
+rm miniconda.sh
+conda update --all
+
+# Conda dev packages
+conda install -y $(echo "
+conda-build
+jinja2
+pip
+setuptools
+")
+
+# Miscellaneous Conda config
+conda config --add envs_dirs /conda/envs
+mkdir /conda/conda-bld
+(cd /conda/conda-bld && ln -s /work/linux-64 linux-64)
+
+# Docker infrastructure cleanup
+chmod +x /entrypoint.sh
+rm /setup.sh # self-destruct!
