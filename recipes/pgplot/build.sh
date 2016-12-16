768e338ed5f9faab777bdb7469ad32efae632eeb
Peter Williams <peter@newton.cx>
2016-12-16 11:02:13 -0500

    recipes/pgplot: abandon Tk support since it's now getting compile errors
    Presumably something related to the conda-forge version of it. Whatever.

--- recipes/pgplot/build.sh
+++ recipes/pgplot/build.sh
@@ -23,12 +23,12 @@ fi
 for f in png.h pngconf.h pnglibconf.h zlib.h zconf.h ; do
     ln -s $PREFIX/include/$f $f
 done
-make
+make all libcpgplot.a
 
 mkdir -p $PREFIX/bin $PREFIX/lib $PREFIX/include/pgplot $PREFIX/share/pgplot
 cp -a pgxwin_server $PREFIX/bin/
-cp -a libcpgplot.a libpgplot.a libpgplot.$shlib libtkpgplot.a $PREFIX/lib/
+cp -a libcpgplot.a libpgplot.a libpgplot.$shlib $PREFIX/lib/
 cp -a grfont.dat rgb.txt $PREFIX/share/pgplot/
-cp -a cpgplot.h grpckg1.inc pgplot.inc tkpgplot.h $PREFIX/include/pgplot/
+cp -a cpgplot.h grpckg1.inc pgplot.inc $PREFIX/include/pgplot/
 
 # NOTE: do not delete .a files! They're what we provide!
