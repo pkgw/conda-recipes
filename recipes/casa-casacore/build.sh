44444df959c81cfc1e2ef47f4687feb50587a2c5
Peter Williams <peter@newton.cx>
2016-12-22 17:15:51 -0500

    recipes/casa-casacore: first cut at casacore from "new" shared Git repo

---
+++ recipes/casa-casacore/build.sh
@@ -0,0 +1,63 @@
+#! /bin/bash
+# Copyright 2015-2016 Peter Williams and collaborators.
+# This file is licensed under a 3-clause BSD license; see LICENSE.txt.
+
+[ "$NJOBS" = '<UNDEFINED>' -o -z "$NJOBS" ] && NJOBS=1
+set -e
+test $(echo "$PREFIX" |wc -c) -gt 200 # check that we're getting long paths
+
+# We do fancy things with Bash arrays because we need to pass command-line
+# options that contain internal spaces, and with pure string expansions
+# there's no way to avoid them getting split up.
+
+cmake_args=(
+    -DBUILD_PYTHON=OFF
+    -DBUILD_TESTING=OFF
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_COLOR_MAKEFILE=OFF
+    -DCMAKE_INSTALL_PREFIX=$PREFIX
+    -DDATA_DIR=$PREFIX/lib/casa/data
+    -DUSE_FFTW3=ON
+    -DUSE_HDF5=ON
+    -DUSE_THREADS=ON
+    -DCXX11=ON
+    -DCONDA_CASA_ROOT=$PREFIX/lib/casa
+)
+
+#cmake_args+=(--debug-trycompile --debug-output)
+
+if [ -n "$OSX_ARCH" ] ; then
+    # Need to require 10.7 because of the C++11 features.
+    export MACOSX_DEPLOYMENT_TARGET=10.7
+    cmake_args+=(
+	-DCMAKE_CXX_FLAGS="-arch $OSX_ARCH -stdlib=libc++ -std=c++11"
+	-DCMAKE_Fortran_COMPILER=/usr/local/bin/gfortran-4.2
+	-DCMAKE_OSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET
+	-DCMAKE_OSX_SYSROOT=/
+	-DREADLINE_INCLUDE_DIR=$PREFIX/include
+	-DREADLINE_LIBRARY=$PREFIX/lib/libreadline.dylib
+    )
+else
+    # Downstream elements of the C++ code require C++11 to compile, and for
+    # binary compatibility we need to compile casacore with C++11 as well. We
+    # can do this even on a CentOS 5 machine thanks to the Red Hat
+    # "devtoolset" package. Newer condas also provide the libgfortran.so.3
+    # required by the matching gfortran compiler.
+    toolroot=/opt/rh/devtoolset-2/root
+
+    cmake_args+=(
+	-DBLAS_LIBRARIES="$PREFIX/lib/libopenblas.so"
+	-DCMAKE_C_COMPILER=$toolroot/usr/bin/gcc
+	-DCMAKE_CXX_COMPILER=$toolroot/usr/bin/g++
+	-DCMAKE_Fortran_COMPILER=$toolroot/usr/bin/gfortran
+    )
+fi
+
+mkdir build
+cd build
+cmake "${cmake_args[@]}" ..
+make -j$NJOBS VERBOSE=1
+make install
+
+cd $PREFIX
+rm -f casa_sover.txt bin/casacore_floatcheck bin/countcode
