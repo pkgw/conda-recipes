3ede968c3ea555861fa46d1391c1d957e603d168
Peter Williams <peter@newton.cx>
2017-01-17 11:32:19 -0500

    Merge py2cairo and py3cairo into pycairo, because seriously
    Also this is what "defaults" has done.

--- recipes/pycairo/build.sh
+++ recipes/pycairo/build.sh
@@ -6,22 +6,19 @@
 set -e
 test $(echo "$PREFIX" |wc -c) -gt 200 # check that we're getting long paths
 
-if [ -z "$OSX_ARCH" ] ; then
-    echo only build me for os x
-    exit 1
-fi
-
 # don't get locally installed pkg-config entries:
 export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig"
 
-# For reasons that completely escape me, if we run "waf configure" with
-# LDFLAGS etc. set, the compile flags returned by Python on OS X include
-# various -isysroot flags pointing to SDKs that we don't have. If they're
-# unset, some code kicks in that strips out these bad flags (cf.
-# _osx_support.py in the Python tree). So let's unset them.
-export MACOSX_DEPLOYMENT_TARGET=10.6
-sdk=/
-unset CFLAGS CXXFLAGS LDFLAGS
+if [ -n "$OSX_ARCH" ] ; then
+    # For reasons that completely escape me, if we run "waf configure" with
+    # LDFLAGS etc. set, the compile flags returned by Python on OS X include
+    # various -isysroot flags pointing to SDKs that we don't have. If they're
+    # unset, some code kicks in that strips out these bad flags (cf.
+    # _osx_support.py in the Python tree). So let's unset them.
+    export MACOSX_DEPLOYMENT_TARGET=10.6
+    sdk=/
+    unset CFLAGS CXXFLAGS LDFLAGS
+fi
 
 ./waf --help >/dev/null # trigger unpacking of waflib directory
 rdir=$(cd $RECIPE_DIR && pwd)
