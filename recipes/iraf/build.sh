eff415e957d08ef615c3f476595067f42f09afbf
Peter Williams <peter@newton.cx>
2017-02-08 22:16:47 -0500

    recipes/iraf: work on getting this building again
    Including the port from modern-xorg-stack to conda-forge X packages.

--- recipes/iraf/build.sh
+++ recipes/iraf/build.sh
@@ -4,6 +4,7 @@
 
 [ "$NJOBS" = '<UNDEFINED>' -o -z "$NJOBS" ] && NJOBS=1
 set -e
+set -o pipefail # we have that key "|tee" command ...
 test $(echo "$PREFIX" |wc -c) -gt 200 # check that we're getting long paths
 
 if [ -n "$OSX_ARCH" ] ; then
