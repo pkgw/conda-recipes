0a34e57613592118ad14c7136f9423d06dbb96d1
Peter Williams <peter@newton.cx>
2017-02-09 20:32:53 -0500

    recipes/pycairo: fix build on OSX/Python 3.x

--- recipes/pycairo/build.sh
+++ recipes/pycairo/build.sh
@@ -23,7 +23,9 @@ fi
 ./waf --help >/dev/null # trigger unpacking of waflib directory
 rdir=$(cd $RECIPE_DIR && pwd)
 (cd .waf* && patch -p1 -i $rdir/60_python-config-without-interpreter.patch)
-(cd .waf* && patch -p1 -i $rdir/70_dont-link-libpython.patch)
+# This patch breaks the build on 3.x on OSX. Not clear why it was put in
+# so let's just ditch it.
+#(cd .waf* && patch -p1 -i $rdir/70_dont-link-libpython.patch)
 (cd .waf* && patch -p1 -i $rdir/80_fix-pickle.patch)
 (cd .waf* && patch -p1 -i $rdir/81_pickling-again.patch)
 ./waf configure --prefix=$PREFIX
