f6bf8ffc9d779d5da3d271151ed5ffbe0842c9f7
Peter Williams <peter@newton.cx>
2017-03-28 17:20:02 -0400

    recipes/casa-tools: work-in-progress on 4.7.2
    I am having super obnoxious issues on OSX and I'm going to commit this work
    so I can try debugging on my non-headless OSX machine.

--- recipes/casa-tools/build.sh
+++ recipes/casa-tools/build.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
-# Copyright 2015-2016 Peter Williams and collaborators.
+# Copyright 2015-2017 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
 [ "$NJOBS" = '<UNDEFINED>' -o -z "$NJOBS" ] && NJOBS=1
 set -e
 test $(echo "$PREFIX" |wc -c) -gt 200 # check that we're getting long paths
 
-export PATH="$PREFIX/lib/qt4/bin:$PATH"
+export PATH="$PREFIX/qt4/bin:$PATH"
 
 (cd $PREFIX/include && ln -s xercesc28 xercesc) || exit 1
 if [ -n "$OSX_ARCH" ] ; then
@@ -20,24 +20,27 @@ cmake_args=(
     -DCMAKE_COLOR_MAKEFILE=OFF
     -DCMAKE_INSTALL_PREFIX=$PREFIX
     -DPGPLOT_INCLUDE_DIRS=$PREFIX/include/pgplot
-    -DQT_DBUSXML2CPP_EXECUTABLE=$PREFIX/lib/qt4/bin/qdbusxml2cpp
-    -DQT_LIBRARY_DIR=$PREFIX/lib/qt4
-    -DQT_MKSPECS_DIR=$PREFIX/share/qt4/mkspecs
-    -DQT_RCC_EXECUTABLE=$PREFIX/lib/qt4/bin/rcc
+    -DQT_DBUSXML2CPP_EXECUTABLE=$PREFIX/qt4/bin/qdbusxml2cpp
+    -DQT_LIBRARY_DIR=$PREFIX/qt4/lib
+    -DQT_MKSPECS_DIR=$PREFIX/qt4/mkspecs
+    -DQT_RCC_EXECUTABLE=$PREFIX/qt4/bin/rcc
     -DQWT_INCLUDE_DIRS=$PREFIX/include/qwt5
 )
 
-#cmake_args+=(--debug-trycompile --debug-output)
+cmake_args+=(--debug-trycompile --debug-output)
 
-if [ -n "$OSX_ARCH" ] ; then
+if [ $(uname) = Darwin ] ; then
     # Need to require 10.7 because of the C++11 features.
     export MACOSX_DEPLOYMENT_TARGET=10.7
+
+    export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
+    linkflags="$LDFLAGS"
 
     cmake_args+=(
 	-Darch=darwin64
 	-Darchflag=x86_64
 	-DCMAKE_CXX_FLAGS="-arch $OSX_ARCH -stdlib=libc++ -std=c++11"
-	-DCMAKE_Fortran_COMPILER=/usr/local/bin/gfortran-4.2
+	-DCMAKE_Fortran_COMPILER=gfortran
 	-DCMAKE_OSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET
 	-DCMAKE_OSX_SYSROOT=/
 	-DPGPLOT_LIBRARIES="$PREFIX/lib/libpgplot.dylib;$PREFIX/lib/libcpgplot.a"
@@ -48,19 +51,24 @@ if [ -n "$OSX_ARCH" ] ; then
     )
 else
     toolroot=/opt/rh/devtoolset-2/root
+    linkflags=-Wl,-rpath-link,$PREFIX/lib
 
     cmake_args+=(
 	-DBLAS_LIBRARIES="$PREFIX/lib/libopenblas.so"
 	-DCMAKE_C_COMPILER=$toolroot/usr/bin/gcc
 	-DCMAKE_CXX_COMPILER=$toolroot/usr/bin/g++
-	-DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath-link,$PREFIX/lib
+	-DCMAKE_CXX_FLAGS="-std=c++11"
 	-DCMAKE_Fortran_COMPILER=$toolroot/usr/bin/gfortran
-	-DCMAKE_MODULE_LINKER_FLAGS=-Wl,-rpath-link,$PREFIX/lib
-	-DCMAKE_SHARED_LINKER_FLAGS=-Wl,-rpath-link,$PREFIX/lib
 	-DLAPACK_LIBRARIES="$PREFIX/lib/libopenblas.so"
 	-DPGPLOT_LIBRARIES="$PREFIX/lib/libpgplot.so;$PREFIX/lib/libcpgplot.a"
     )
 fi
+
+cmake_args+=(
+    -DCMAKE_EXE_LINKER_FLAGS="$linkflags"
+    -DCMAKE_MODULE_LINKER_FLAGS="$linkflags"
+    -DCMAKE_SHARED_LINKER_FLAGS="$linkflags"
+)
 
 cd code
 mkdir build
