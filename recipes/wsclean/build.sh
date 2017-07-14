2c76d103174f30a61d82d8b099e7127a29d821aa
Peter Williams <peter@newton.cx>
2017-07-14 10:23:54 -0400

    wsclean: new recipe

---
+++ recipes/wsclean/build.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+# Copyright 2015-2016 Peter Williams and collaborators.
+# This file is licensed under a 3-clause BSD license; see LICENSE.txt.
+
+[ "$NJOBS" = '<UNDEFINED>' -o -z "$NJOBS" ] && NJOBS=1
+set -e
+test $(echo "$PREFIX" |wc -c) -gt 200 # check that we're getting long paths
+
+cmake_args=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_COLOR_MAKEFILE=OFF
+    -DCMAKE_INSTALL_PREFIX=$PREFIX
+    -DPORTABLE=ON
+)
+
+#cmake_args+=(--debug-trycompile --debug-output)
+
+if [ -n "$OSX_ARCH" ] ; then
+    # Need to require 10.7 because of the C++11 features.
+    export MACOSX_DEPLOYMENT_TARGET=10.7
+
+    cmake_args+=(
+	-Darch=darwin64
+	-Darchflag=x86_64
+	-DCMAKE_CXX_FLAGS="-arch $OSX_ARCH -stdlib=libc++ -std=c++11"
+	-DCMAKE_Fortran_COMPILER=/usr/local/bin/gfortran-4.2
+	-DCMAKE_OSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET
+	-DCMAKE_OSX_SYSROOT=/
+    )
+else
+    toolroot=/opt/rh/devtoolset-2/root
+
+    cmake_args+=(
+	-DCMAKE_C_COMPILER=$toolroot/usr/bin/gcc
+	-DCMAKE_CXX_COMPILER=$toolroot/usr/bin/g++
+	-DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath-link,$PREFIX/lib
+	-DCMAKE_MODULE_LINKER_FLAGS=-Wl,-rpath-link,$PREFIX/lib
+	-DCMAKE_SHARED_LINKER_FLAGS=-Wl,-rpath-link,$PREFIX/lib
+    )
+fi
+
+mkdir build
+cd build
+cmake "${cmake_args[@]}" ..
+make -j$NJOBS VERBOSE=1 wsclean
+make install
+
+cd $PREFIX
+find . -name '*.a' -delete
