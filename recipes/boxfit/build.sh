fbf1fd1ab92030a8e406d713f5a830a270c3af6d
Peter Williams <peter@newton.cx>
2017-09-05 14:55:07 -0400

    boxfit: build and provide both boost and no-boost program variants

--- recipes/boxfit/build.sh
+++ recipes/boxfit/build.sh
@@ -6,13 +6,22 @@
 set -e
 test $(echo "$PREFIX" |wc -c) -gt 200 # check that we're getting long paths
 
-cd src
-make all
-
 mkdir -p $PREFIX/bin $PREFIX/share/boxfit
 
+# We compile and install two versions since the program's capability to deal
+# with Doppler boosting is, annoyingly, a compile-time flag.
+
+cd src
+mkdir -p bin
+make all HOME=.
+cp -p ../bin/boxfit $PREFIX/bin/boxfit-boost
+cp -p ../bin/dump_box $PREFIX/bin/
+rm -f ../bin/* *.o
+sed -i -e 's|#define BOOST_.*|#define BOOST_ DISABLED_|' environment.h
+make all HOME=.
+cp -p ../bin/boxfit $PREFIX/bin/boxfit-noboost
+
 cd ..
-cp -p bin/boxfit bin/dump_box $PREFIX/bin/
 for f in settings/*.txt ; do
     sed -e "s|@datadir@|$PREFIX/share/boxfit|" $f >$PREFIX/share/boxfit/$(basename $f)
 done
