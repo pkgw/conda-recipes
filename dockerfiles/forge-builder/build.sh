ed84d339c6bc01184b5cb85b375e24414741f72e
Peter Williams <peter@newton.cx>
2019-07-12 22:16:14 -0400

    dockerfiles/forge-builder/build.sh: let this be run from any directory

--- dockerfiles/forge-builder/build.sh
+++ dockerfiles/forge-builder/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-# Copyright 2015-2018 Peter Williams
+# Copyright 2015-2019 Peter Williams
 # Licensed under the MIT License.
 
 set -e
-exec docker build --network=host --build-arg=EXTUSERID=$(id -u) --build-arg=EXTGRPID=$(id -g) -t forge-builder .
+exec docker build --network=host --build-arg=EXTUSERID=$(id -u) --build-arg=EXTGRPID=$(id -g) -t forge-builder $(dirname $0)
