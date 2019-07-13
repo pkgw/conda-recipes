b3873186bb40dd20816b2b6f394c7aaab971a9e7
Peter Williams <peter@newton.cx>
2019-07-13 01:29:17 -0400

    gtk3: start work towards using version 3.24
    The new version forces us to switch to the Meson build system. I'm also adding
    Wayland support, since I want my windows to stop being too big on my HiDPI
    displays.
    
    Currently we have a hack in the meta.yaml to hardcode the Pango version, since
    this version of Gtk+3 requires a Pango newer than what conda-forge provides.
    This will need fixing since so many other packages rely on Pango as well.
    
    I also have not built on macOS yet.

--- recipes/gtk3/build.sh
+++ recipes/gtk3/build.sh
@@ -1,41 +1,28 @@
 #! /bin/bash
-# Copyright 2014-2017 Peter Williams and collaborators.
+# Copyright 2014-2019 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
 [ "$NJOBS" = '<UNDEFINED>' -o -z "$NJOBS" ] && NJOBS=1
-set -e
+set -ex
 
-configure_args=(
-    --prefix=$PREFIX
-    --x-includes=$PREFIX/include
-    --x-libraries=$PREFIX/lib
-    --disable-gtk-doc
+meson_config_args=(
+    -D gtk_doc=false
+    -D demos=false
+    -D examples=false
+    -D installed_tests=false
+    -D wayland_backend=true
 )
 
 if [ -n "$OSX_ARCH" ] ; then
-    export MACOSX_DEPLOYMENT_TARGET=10.9
-
-    # rpath setting is often needed to run compiled autoconf test programs:
-    export LDFLAGS="$LDFLAGS -Wl,-rpath,$PREFIX/lib"
-
-    configure_args+=(
-	--enable-quartz-backend
-	--disable-cups
-    )
-
     # Clashing libuuid causes compilation problems unless we do this. libuuid
     # is pulled in by xorg-libSM.
     rm -rf $PREFIX/include/uuid
-else
-    # I get a double-free crash in gtk/gtkcairoblur.c if I compile with the
-    # stock compilers, and it goes away if I use the updated ones!
-    export PATH=/opt/rh/devtoolset-7/root/usr/bin:$PATH
 fi
 
-rm -f $PREFIX/lib/*.la # deps include busted libtool files
-./configure "${configure_args[@]}" || { cat config.log ; exit 1 ; }
-make -j$NJOBS
-make install
+meson builddir --prefix=$PREFIX --libdir=$PREFIX/lib
+meson configure "${meson_config_args[@]}" builddir
+ninja -v -C builddir
+ninja -C builddir install
 
 cd $PREFIX
 find . '(' -name '*.la' -o -name '*.a' ')' -delete
