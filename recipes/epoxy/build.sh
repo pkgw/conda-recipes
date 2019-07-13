45ef9fa453c8527456d7941832fcdfd75bdfba55
Peter Williams <peter@newton.cx>
2019-07-13 01:27:06 -0400

    epoxy: update to 1.5.3 and add EGL
    Latest GTK+3 wants epoxy to have EGL support. This turned out to be super
    hard! I have to install CDTs at build time and trick the pkg-config into
    paying attention to them. Then I have to *not* reference those dependencies in
    the pkg-config file we emit, otherwise it won't be usable on builds without
    the magic CDT/pkg-config setup. And I also have to copy headers that they use
    for similar reasons. Plus the build system switched to Meson.

--- recipes/epoxy/build.sh
+++ recipes/epoxy/build.sh
@@ -1,28 +1,31 @@
 #! /bin/bash
-# Copyright 2017 Peter Williams and collaborators.
+# Copyright 2017-2019 Peter Williams and collaborators.
 # This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
 [ "$NJOBS" = '<UNDEFINED>' -o -z "$NJOBS" ] && NJOBS=1
-set -e
+set -ex
 
-configure_args=(
-    --prefix=$PREFIX
-    --x-includes=$PREFIX/include
-    --x-libraries=$PREFIX/lib
-    --disable-egl
+meson_config_args=(
+    -D docs=false
+    -D egl=yes
+    -D x11=true
+    -D tests=false
 )
 
-if [ -n "$OSX_ARCH" ] ; then
-    export MACOSX_DEPLOYMENT_TARGET=10.9
-    export LDFLAGS="$LDFLAGS -Wl,-rpath,$PREFIX/lib"
-else
-    # also for X11:
-    export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$PREFIX/lib"
-fi
+# Make it so that pkg-config can find the CDT (E)GL(X) packages:
+export PKG_CONFIG_PATH="$BUILD_PREFIX/$HOST/sysroot/usr/lib64/pkgconfig"
 
-./configure "${configure_args[@]}" || { cat config.log ; exit 1 ; }
-make -j$NJOBS
-make install
+# Hackity hack: manually copy the GL headers that we require, so that
+# downstream deps don't all have to include a magic CDT package as a dependency
+
+mkdir -p $PREFIX/include/KHR $PREFIX/include/EGL
+cp $BUILD_PREFIX/$HOST/sysroot/usr/include/KHR/khrplatform.h $PREFIX/include/KHR/
+cp $BUILD_PREFIX/$HOST/sysroot/usr/include/EGL/eglplatform.h $PREFIX/include/EGL/
+
+meson builddir --prefix=$PREFIX --libdir=$PREFIX/lib
+meson configure "${meson_config_args[@]}" builddir
+ninja -v -C builddir
+ninja -C builddir install
 
 cd $PREFIX
 find . '(' -name '*.la' -o -name '*.a' ')' -delete
