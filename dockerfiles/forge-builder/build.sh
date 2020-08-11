0c5b12400e7c5a897ff040e8c491e58931b2635b
Peter Williams <peter@newton.cx>
2020-08-11 12:56:33 -0400

    dockerfiles/forge-builder/build.sh: document weird build errors

--- dockerfiles/forge-builder/build.sh
+++ dockerfiles/forge-builder/build.sh
@@ -3,4 +3,10 @@
 # Licensed under the MIT License.
 
 set -e
+
+# XXXX: errors building the image related to resource limits, unless I change
+# the user/group ID away from those of my user account? Mysterious but I worked
+# around by just replace $(id -u) with 1111, etc.
+
 exec docker build --network=host --build-arg=EXTUSERID=$(id -u) --build-arg=EXTGRPID=$(id -g) -t forge-builder $(dirname $0)
+
